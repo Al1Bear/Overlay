4437d51aa60acb24b0e29cfcd176fec9f7d94f74
Al1Bear <102313493+Al1Bear@users.noreply.github.com>
2025-08-18 12:25:53 +0100

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,29 +1,26 @@
 // preload.js
 const { contextBridge, ipcRenderer } = require('electron');
 
-contextBridge.exposeInMainWorld('bridge', {
-  // ROI
-  getRoi:     () => ipcRenderer.invoke('roi:get'),
-  setRoi:     (b) => ipcRenderer.invoke('roi:set', b),
-  editRoi:    (on) => ipcRenderer.invoke('roi:edit', !!on),
-  toggleBox:  () => ipcRenderer.invoke('roi:toggle-visible'),
+const api = {
+  roi: {
+    get: () => ipcRenderer.invoke('roi:get'),
+    set: (rect) => ipcRenderer.invoke('roi:set', rect),
+    edit: () => ipcRenderer.invoke('roi:edit'),
+    toggleVisible: () => ipcRenderer.invoke('roi:toggle-visible'),
+    onBounds: (cb) => ipcRenderer.on('roi:bounds', (_e, rect) => cb(rect)),
+    onEditing: (cb) => ipcRenderer.on('roi:editing', (_e, on) => cb(on)),
+    onVisible: (cb) => ipcRenderer.on('roi:visible', (_e, v) => cb(v)),
+    hello: () => ipcRenderer.send('roi:hello')
+  },
+  snap: () => ipcRenderer.invoke('snap'),
+  auto: {
+    toggle: () => ipcRenderer.invoke('auto'),
+    onState: (cb) => ipcRenderer.on('auto:state', (_e, on) => cb(on)),
+    onCapture: (cb) => ipcRenderer.on('auto:capture', (_e, data) => cb(data))
+  },
+  log: (msg) => ipcRenderer.send('log', msg)
+};
 
-  // Capture / Auto
-  snap:       () => ipcRenderer.invoke('snap'),
-  auto:       (on) => ipcRenderer.invoke('auto', !!on),
-
-  // Sidebar status
-  onUpdate:   (cb) => ipcRenderer.on('side:update', (_e, payload) => cb(payload)),
-  onStatus:   (cb) => ipcRenderer.on('side:status', (_e, s) => cb(s)),
-
-  // Hotkey relays from main
-  onEditToggle: (cb) => ipcRenderer.on('side:editToggle', () => cb()),
-  onSnap:       (cb) => ipcRenderer.on('side:snap', () => cb()),
-  onAutoToggle: (cb) => ipcRenderer.on('side:autoToggle', () => cb()),
-
-  // ROI overlay edit on/off
-  onRoiEdit:  (cb) => ipcRenderer.on('roi:edit', (_e, on) => cb(on)),
-
-  // misc
-  saveText:   (t) => ipcRenderer.invoke('save-text', String(t || '')),
-});
+contextBridge.exposeInMainWorld('api', api);
+// compatibility alias if existing renderer used electronAPI
+contextBridge.exposeInMainWorld('electronAPI', api);
